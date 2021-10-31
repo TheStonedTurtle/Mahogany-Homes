f56083777f5f6d086f77be6167bf53941a26abe6
TheStonedTurtle <mgoodwin559@gmail.com>
2021-10-31 14:39:30 -0700

    Fix RequiredMaterials checkstyle

--- src/main/java/thestonedturtle/mahoganyhomes/RequiredMaterials.java
+++ src/main/java/thestonedturtle/mahoganyhomes/RequiredMaterials.java
@@ -1,16 +1,12 @@
 package thestonedturtle.mahoganyhomes;
 
-public class RequiredMaterials {
+import lombok.AllArgsConstructor;
 
-	public Integer MinPlanks;
-	public Integer MaxPlanks;
-	public Integer MinSteelBars;
-	public Integer MaxSteelBars;
-
-	public RequiredMaterials(int minPlanks, int maxPlanks, int minSteelBars, int maxSteelBars) {
-		MinPlanks = minPlanks;
-		MaxPlanks = maxPlanks;
-		MinSteelBars = minSteelBars;
-		MaxSteelBars = maxSteelBars;
-	}
+@AllArgsConstructor
+public class RequiredMaterials
+{
+	public int MinPlanks;
+	public int MaxPlanks;
+	public int MinSteelBars;
+	public int MaxSteelBars;
 }
